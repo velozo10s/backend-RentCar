49431f120e3dcbc50bac3c73875e63e9d93d76c7
davilozo <davidvelozo2001@gmail.com>
2025-10-23 10:30:31 -0300

    fix(cors): Add domain

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ import logger from "./utils/logger.js";
 import reservationRoutes from "./routes/reservationRoutes.js";
 import ratingRoutes from "./routes/ratingRoutes.js";
 import customerRoutes from "./routes/customerRoutes.js";
+// import reportRoutes from "./routes/reportRoutes.js";
+// import contractRoutes from "./routes/contractRoutes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,7 +25,8 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'http://192.168.100.208:5173'
+    'http://192.168.100.208:5173',
+    'https://web-rentcar-dlpix.ondigitalocean.app'
   ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
@@ -39,6 +42,8 @@ app.use('/api/reservations/', reservationRoutes);
 app.use('/api/employees/', employeeRoutes);
 app.use('/api/customers/', customerRoutes);
 app.use('/api/ratings/', ratingRoutes);
+// app.use('/api/reports/', reportRoutes);
+// app.use('/api/contracts/', contractRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const PORT = process.env.PORT || 5000;
